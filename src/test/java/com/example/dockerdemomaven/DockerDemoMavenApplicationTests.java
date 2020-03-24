589141a9c46e66513eb7a7385d62fefc20a3e801
asad <asad@MDs-MacBook-Pro.local>
2020-03-23 19:23:49 -0500

    Initial commit

---
+++ src/test/java/com/example/dockerdemomaven/DockerDemoMavenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dockerdemomaven;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerDemoMavenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
